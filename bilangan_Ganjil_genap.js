95a7d86cc6f601e2a296b166cf3ed94d76bf8422
lihnty <ahmadpondokit@gmail.com>
2024-10-20 09:44:20 +0800

    bilangan_Ganjil_genap

---
+++ bilangan_Ganjil_genap.js
@@ -0,0 +1,6 @@
+var angka = prompt('masukkan angka :');
+if( angka % 2 === 0) {
+    alert(angka + ' Angka ini adalah bilangan genap');
+}else {
+    alert(angka + ' Angka ini adalah bilangan ganjil');
+}
